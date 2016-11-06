ac526df2cb924f5fbb7ed8be77d507abe5de180e
Daniel Weibel <danielmweibel@gmail.com>
2016-11-06 13:19:35 +0100

    DialogFragment dialog created in onCreateDialog and displayed by show
    This is the most preferred way to create a simple dialog. The creation
    of the dialog works in the same way as creating a dialog directly
    (i.e. with AlertDialog.Builder), but since the dialog is managed by
    the DialogFragment, the dialog survives screen rotations and Activity
    restarts.
    
    A dialog created in onCreateDialog can be only displayed as a dialog
    (i.e. floating window), and not as a Fragment.

--- app/src/main/java/org/weibeld/example/dialogfragmentexample/MainActivity.java
+++ app/src/main/java/org/weibeld/example/dialogfragmentexample/MainActivity.java
@@ -1,12 +1,10 @@
 package org.weibeld.example.dialogfragmentexample;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,34 +17,10 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createAndShowDialog();
+                FragmentManager fm = getFragmentManager();
+                MyDialogFragment dialogFrag = new MyDialogFragment();
+                dialogFrag.show(fm, getString(R.string.dialog_tag));
             }
         });
-    }
-
-    private void createAndShowDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        // Add components to the dialog (the order in which components are added does not matter)
-        builder.setTitle(R.string.dialog_title).
-                setMessage(R.string.dialog_message).
-                setView(R.layout.dialog_partial_content).  // For < API 21 use setView(View)
-                setPositiveButton(R.string.pos_button, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        showToast(getString(R.string.pos_button));
-                    }
-                }).
-                setNegativeButton(R.string.neg_button, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        showToast(getString(R.string.neg_button));
-                    }
-                });
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
-
-    private void showToast(String buttonName) {
-        Toast.makeText(this, "Clicked on \"" + buttonName + "\"", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/org/weibeld/example/dialogfragmentexample/MyDialogFragment.java
@@ -0,0 +1,43 @@
+package org.weibeld.example.dialogfragmentexample;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by dw on 06/11/16.
+ */
+
+public class MyDialogFragment extends DialogFragment {
+
+    // Called after onCreate and before onCreateView
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Add components to the dialog (the order in which components are added does not matter)
+        builder.setTitle(R.string.dialog_title).
+                setMessage(R.string.dialog_message).
+                setView(R.layout.dialog_partial_content).  // For < API 21 use setView(View)
+                setPositiveButton(R.string.pos_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        showToast(getString(R.string.pos_button));
+                    }
+                }).
+                setNegativeButton(R.string.neg_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        showToast(getString(R.string.neg_button));
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+    private void showToast(String buttonName) {
+        Toast.makeText(getActivity(), "Clicked on \"" + buttonName + "\"", Toast.LENGTH_SHORT).show();
+    }
+}
